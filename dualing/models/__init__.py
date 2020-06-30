9c7dedcd7146776575913603999b408044df7df8
Gustavo Rosa <gth.rosa@uol.com.br>
2020-06-30 12:03:03 -0300

    Adding contrastive siamese class.

--- dualing/models/__init__.py
+++ dualing/models/__init__.py
@@ -1,4 +1,5 @@
 """A package for already-implemented machine learning Siamese architectures.
 """
 
+from dualing.models.contrastive import ContrastiveSiamese
 from dualing.models.cross_entropy import CrossEntropySiamese
--- dualing/utils/losses.py
+++ dualing/utils/losses.py
@@ -1,25 +1,32 @@
 import tensorflow as tf
 
 
-def binary_crossentropy(y_true, y_pred):
+def binary_crossentropy(y, pred):
     """Calculates the binary cross-entropy loss.
 
     Args:
-    
+        y (tf.Tensor): Tensor of true labels.
+        pred (tf.Tensor): Tensor of predicted labels.
+
     Returns:
+        Binary cross-entropy loss.
 
     """
 
-    return tf.keras.losses.binary_crossentropy(y_true, y_pred)
+    return tf.keras.losses.binary_crossentropy(y, pred)
 
 
-def contrastive_loss(y_true, y_pred):
+def contrastive_loss(y, pred, margin=1):
     """Calculates the contrastive loss.
 
     Args:
-    
+        y (tf.Tensor): Tensor of true labels.
+        pred (tf.Tensor): Tensor of predicted distances.
+        margin (float): Radius around the space to be computed.
+
     Returns:
-    
+        Contrastive loss.
+
     """
 
-    return tf.math.reduce_mean(y_true * tf.math.square(y_pred) + (1 - y_true) * tf.math.square(tf.math.maximum(1 - y_pred, 0)))
+    return y * tf.math.square(pred) + (1.0 - y) * tf.math.square(tf.math.maximum(margin - pred, 0.0))
